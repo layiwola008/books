cc560b5fe8daec6fbbb7e329a0e314f791e3a7e5
Olayiwola Adepoju <78272392+layiwola008@users.noreply.github.com>
2021-11-01 07:15:54 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>My Book List React App</h1>
+      <h2>Books Coming Soon</h2>
+    </div>
+  );
+}
